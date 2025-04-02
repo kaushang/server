16a3902d737e87dc9c4bafeaf39e89b739e19cbd
Kaushang Surya <kaushangsurya29@gmail.com>
2025-04-02 19:35:15 +0530

    fixes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cookieParser = require("cookie-parser");
 const postModel = require("./models/postModel");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
+const cors = require("cors");
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
